ca6894467801acaa7752b75810bc82d665abc405
Josh Pennington <McMallen1@Joshuas-Mac-mini.local>
2017-04-24 11:20:27 -0400

    created logic for dropdown menu selection

--- app/data/friend.js
+++ app/data/friend.js
@@ -1,9 +1,17 @@
 var peopleArr = [
   {
-    "name":"Ahmed",
+    "name":"Randy Orton",
     "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
     "scores":[
-        5,
+        1,
+        1
+      ]
+  },
+  {
+    "name":"Sandy Andy",
+    "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores":[
+        2,
         1
       ]
   }
