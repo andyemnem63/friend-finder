f7ced8132ce0bc9052726b034b0dd14c92a3ec9d
Josh Pennington <McMallen1@Joshuas-Mac-mini.local>
2017-04-26 17:51:20 -0400

    attempted to link css files

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ var express = require('express'),
 	app = express(); 
 
 // Sets an initial port. We"ll use this later in our listener
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 // middleware parses data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
-
+app.use(express.static(__dirname + '/public'));
 // Require Routes
 //App is passed in because it lets use us express in other files
 require('./app/routing/htmlRoutes.js')(app);
@@ -21,3 +21,5 @@ require('./app/routing/apiRoutes.js')(app);
 app.listen(PORT,function() {
 	console.log('Listening for App' , PORT);
 });
+
+
