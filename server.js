f108fddc0aa805029daf5c468edbf8d4cecd9140
AndyEm <andyem64@gmail.com>
2017-04-27 13:56:11 -0400

    Changed questions

--- server.js
+++ server.js
@@ -1,8 +1,7 @@
 // Modules 
 var express = require('express'),
 	bodyParser = require('body-parser'),
-	app = express(); 
-
+	app = express();
 // Sets an initial port. We"ll use this later in our listener
 const PORT = process.env.PORT || 5000;
 // middleware parses data
@@ -11,6 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 app.use(express.static(__dirname + '/public'));
+
 // Require Routes
 //App is passed in because it lets use us express in other files
 require('./app/routing/htmlRoutes.js')(app);
